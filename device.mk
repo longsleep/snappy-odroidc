9fe4b021c892b7ad1dcda318e8a4f343d9ea0877
Simon Eisenmann <simon@longsleep.org>
2015-09-19 17:14:03 +0200

    Include Linux firmware files in device tarball.

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ DEVICE_INITRD_IMG := $(DEVICE_SRC)/initrd.img
 DEVICE_UINITRD := $(DEVICE_SRC)/assets/uInitrd
 DEVICE_MODULES := $(DEVICE_SRC)/system
 DEVICE_MODPROBE_D := $(DEVICE_SRC)/system/lib/modprobe.d
+DEVICE_FIRMWARE := $(DEVICE_SRC)/system/lib/firmware
 DEVICE_TAR := $(PWD)/device-odroidc_$(DEVICE_VERSION).tar.xz
 
 all: build
@@ -39,7 +40,7 @@ $(DEVICE_UINITRD): $(DEVICE_INITRD_IMG)
 	@rm -f $(DEVICE_UINITRD)
 	mkimage -A arm -T ramdisk -C none -n "Snappy Initrd" -d $(DEVICE_INITRD_IMG) $(DEVICE_UINITRD)
 
-$(DEVICE_INITRD_IMG): $(DEVICE_SRC)/preinstalled/initrd.img
+$(DEVICE_INITRD_IMG): preinstalled
 	@rm -f $(DEVICE_INITRD_IMG)
 	@rm -rf $(DEVICE_INITRD)
 	@mkdir -p $(DEVICE_INITRD)
@@ -51,10 +52,11 @@ $(DEVICE_INITRD_IMG): $(DEVICE_SRC)/preinstalled/initrd.img
 $(DEVICE_SRC)/preinstalled.tar.gz: | $(DEVICE_SRC)
 	@wget $(DEVICE_PREINSTALLED) -O $@
 
-$(DEVICE_SRC)/preinstalled/initrd.img: $(DEVICE_SRC)/preinstalled.tar.gz
+preinstalled: $(DEVICE_SRC)/preinstalled.tar.gz
+	@rm -rf $(DEVICE_SRC)/preinstalled
 	@mkdir -p $(DEVICE_SRC)/preinstalled
-	tar xzvf $< -C $(DEVICE_SRC)/preinstalled --wildcards 'system/boot/initrd.img-*'
-	cp $(DEVICE_SRC)/preinstalled/system/boot/initrd.img-* $@
+	tar xzvf $< -C $(DEVICE_SRC)/preinstalled --wildcards 'system/boot/initrd.img-*' --wildcards 'system/lib/firmware/*'
+	cp $(DEVICE_SRC)/preinstalled/system/boot/initrd.img-* $(DEVICE_SRC)/preinstalled/initrd.img
 
 dtbs:
 	@if [ ! -f $(LINUX_DTB) ] ; then echo "Build linux first."; exit 1; fi
@@ -72,10 +74,13 @@ modprobe.d:
 	@mkdir -p $(DEVICE_MODPROBE_D)
 	cp -a $(DEVICE_SRC)/modprobe.d/* $(DEVICE_MODPROBE_D)
 
-device: $(DEVICE_UIMAGE) $(DEVICE_UINITRD) dtbs modules modprobe.d
+firmware:
+	rsync -rva --exclude "*-generic" $(DEVICE_SRC)/preinstalled/system/lib/firmware/ $(DEVICE_FIRMWARE)/
+
+device: $(DEVICE_UIMAGE) $(DEVICE_UINITRD) dtbs modules modprobe.d firmware
 	@rm -f $(DEVICE_TAR)
 	tar -C $(DEVICE_SRC) -cavf $(DEVICE_TAR) --exclude ./preinstalled --exclude ./preinstalled.tar.gz --exclude ./initrd --exclude ./initrd.img --exclude ./modprobe.d --xform s:'./':: .
 
 build: device
 
-.PHONY: dtbs modules device build $(DEVICE_INITRD_IMG) $(DEVICE_UIMAGE)
+.PHONY: preinstalled dtbs modules device build $(DEVICE_INITRD_IMG) $(DEVICE_UIMAGE)
